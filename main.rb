2517102e4e025b2201f6dbe25031d0cd88f2efc3
pure-adachi <takuya_adachi@pure-system.com>
2021-06-02 02:08:17 +0900

    DISCORD_BOT_TOKEN

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: ENV['TOKEN']
+bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
 
 bot.command :choose! do |event|
   event.respond 'TEST'
