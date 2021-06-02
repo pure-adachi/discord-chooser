2858b1c43edd5a0592a69c73e6c28538db113a54
pure-adachi <takuya_adachi@pure-system.com>
2021-06-02 22:31:10 +0900

    js

--- main.rb
+++
@@ -1,9 +0,0 @@
-require 'discordrb'
-
-bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
-
-bot.command :choose! do |event|
-  event.respond 'TEST'
-end
-
-bot.run
---
+++ server.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+
+client.on("ready", () => {
+  client.user.setPresence({ game: { name: "choose!" } });
+});
+
+client.on("message", (message) => {
+  const memtion = message.mentions.members.first();
+
+  if (!memtion) return;
+  if (memtion.user.id !== client.user.id) return;
+  if (message.author.id === client.user.id) return;
+
+  const channels = message.guild.channels;
+  const voiceChannel = channels.find((ch) => {
+    if (ch.type !== "voice") {
+      return false;
+    }
+
+    return ch.members.some((member) => member.user.id === message.author.id);
+  });
+
+  if (voiceChannel) {
+    const members = voiceChannel.members.map((member) => member.user.username);
+    const length = members.length;
+    const rand = Math.floor(Math.random() * length);
+
+    message.channel.send(`選ばれたのは ${members[rand]} さんです。`);
+  } else {
+    message.reply("ボイスチャンネルに入室しましょう");
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
